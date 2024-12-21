a163d2a0d863946036a46ba9c442d93f36feae38
kavyaparvatham04 <kavyaparvatham46@gmail.com>
2024-12-21 10:10:17 +0530

    Update the header and make the profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar:{
         type: String,
-        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw1ARdRgU0uP1DKlgU6H36Rn&ust=1734330673474000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIjovq-TqYoDFQAAAAAdAAAAABAE"
+        default: "https://www.bing.com/images/search?view=detailV2&id=5D854DEF13740AB8B312F36E571C24284A960A5C&thid=OIP.hqVxcc6_LqTo11wCPWw3JgHaE8&mediaurl=https%3a%2f%2fshotkit.com%2fwp-content%2fuploads%2f2021%2f06%2fCool-profile-picture-Instagram.jpg&exph=580&expw=870&q=profile+picture+ideas&simid=608029011986763267&ck=AD72F24DAD4DB591EA17927BF1ED9AC4&itb=0&FORM=IVCLIGhttps://th.bing.com/th/id/OIP.qgm73u6qEWovC4xX8DknSQHaHa?w=201&h=201&c=7&r=0&o=5&dpr=1.3&pid=1.7"
     },
 }, {timestamps : true} );
 
